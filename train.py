91620a2646fa3680d2d1ae7c4484c874d9d9cdeb
HarbingerOfYeet <winston.ngo.den@gmail.com>
2023-03-29 21:27:30 -0400

    Updated train script

--- train.py
+++ train.py
@@ -2,10 +2,13 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as f
+import torch.nn.functional as F
 
 class Net(nn.Module):
+
     def __init__(self):
-        super().__init__()
+        super(Net, self).__init__()
+        self.linear = nn.Linear()
+        
 
     #def forward(self, x):
